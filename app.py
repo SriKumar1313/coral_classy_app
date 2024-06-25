b2481690d721091c49cb468e3fe1b14019a28615
SriKumar1313 <149764826+SriKumar1313@users.noreply.github.com>
2024-06-25 11:26:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def main():
         f"""
         <style>
         .stApp {{
-            background-image: url('https://github.com/SriKumar1313/coral_classy_app/blob/main/assets/background.png?raw=true');
+            background-image: url('https://github.com/SriKumar1313/coral_classy_app/blob/main/assets/background.jpg?raw=true');
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
