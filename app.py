89486ff1dd11db9e80093bfd2c5704a17aab00fe
SriKumar1313 <149764826+SriKumar1313@users.noreply.github.com>
2024-06-25 11:58:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,17 @@ from PIL import Image, ImageOps
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
-# Function to preprocess the uploaded image
 def preprocess_image(image, img_size=(64, 64)):
     img = ImageOps.fit(image, img_size, Image.LANCZOS)  # Resize using LANCZOS method
     img = ImageOps.grayscale(img)  # Convert to grayscale
     img_array = np.array(img).flatten()
     return img_array
 
-# Function to load the SVM model and preprocessing components
 def load_model(model_path):
     with open(model_path, 'rb') as file:
         svm_model, scaler, pca = pickle.load(file)
     return svm_model, scaler, pca
 
-# Function to make predictions
 def predict(image, model, scaler, pca):
     img_array = preprocess_image(image)
     img_scaled = scaler.transform(img_array.reshape(1, -1))
@@ -28,7 +25,6 @@ def predict(image, model, scaler, pca):
     prediction = model.predict(img_pca)
     return prediction
 
-# Main function to run the Streamlit web app
 def main():
     st.set_page_config(
         page_title="Coral Reef Image Classifier App",
@@ -37,7 +33,6 @@ def main():
         initial_sidebar_state="expanded",
     )
 
-    # Adding background image and custom CSS
     st.markdown(
         f"""
         <style>
@@ -93,7 +88,7 @@ def main():
     st.sidebar.title("📤 Upload Your Image Here")
     st.sidebar.write("Please upload an image of coral reefs to start the classification.")
 
-    # Upload image
+
     uploaded_file = st.sidebar.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
@@ -101,7 +96,6 @@ def main():
         st.sidebar.write("")
         st.sidebar.write("Classifying...")
 
-        # Load SVM model and preprocessing components
         model_path = 'svm_model_pca.pkl'
         if not os.path.exists(model_path):
             st.error("Error: SVM model file not found.")
@@ -109,7 +103,6 @@ def main():
         
         svm_model, scaler, pca = load_model(model_path)
 
-        # Add progress bar and status text
         with st.spinner("Processing..."):
             prediction = predict(image, svm_model, scaler, pca)
 
@@ -134,7 +127,6 @@ def main():
 def create_confetti():
     import pydeck as pdk
 
-    # Data to create confetti effect
     data = pd.DataFrame({
         'lat': np.random.uniform(-90, 90, 500),
         'lon': np.random.uniform(-180, 180, 500),
